1ca817df820a3735672105cc9058e6d5bfb1c2cd
AshesHub <69826743+AshesHub@users.noreply.github.com>
2025-04-23 19:52:54 +1000

    Update build_wine.sh

--- build_wine.sh
+++ build_wine.sh
@@ -100,7 +100,7 @@ export CROSSCC_X64="x86_64-w64-mingw32-gcc"
 export CROSSCXX_X64="x86_64-w64-mingw32-g++"
 
 export CFLAGS_X32="-march=i686 -msse2 -mfpmath=sse -O2 -ftree-vectorize"
-export CFLAGS_X64="-march=x86-64 -msse3 -mfpmath=sse -O2 -ftree-vectorize"
+export CFLAGS_X64="-march=znver5 -msse3 -mfpmath=sse -O2 -ftree-vectorize"
 export LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
 
 export CROSSCFLAGS_X32="${CFLAGS_X32}"
